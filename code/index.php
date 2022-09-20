7c3274f77df60e1e0e5dbe3225f279ecf4f2afe0
zaaalex <alexzorko@yandex.ru>
2022-09-20 21:15:50 +0200

    Умножение и деление

--- code/index.php
+++ code/index.php
@@ -1,4 +1,6 @@
 <?php
-    $last_month=1187.23;
-    $this_month=1089.98;
-echo $last_month-$this_month;
+$num_languages=4;
+$months=11;
+$days=$months*16;
+$days_per_language=$days/$num_languages;
+echo $days_per_language;
