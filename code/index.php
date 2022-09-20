7993f2a42454aa12d5895a36f51bc7086210b83a
zaaalex <alexzorko@yandex.ru>
2022-09-20 21:13:17 +0200

    Числа

--- code/index.php
+++ code/index.php
@@ -1,8 +1,4 @@
 <?php
-/* Imagine a lot of code here */
-$very_bad_unclear_name = "15 chicken wings";
-// Write your code here:
-$order=&$very_bad_unclear_name;
-$order.=" complete";
-// Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+    $last_month=1187.23;
+    $this_month=1089.98;
+echo $last_month-$this_month;
