eabd6465d0b7e43d587845f355e20fceb6ec7bc4
zaaalex <alexzorko@yandex.ru>
2022-09-24 10:21:14 +0200

    Функции 3

--- code/index.php
+++ code/index.php
@@ -1,8 +1,8 @@
 <?php
 
-function increaseEnthusiasm($s){
-    return str_pad($s,strlen($s)+1, "!");
+function repeatThreeTimes ($s){
+    return str_repeat($s,3);
 }
 
-$n=increaseEnthusiasm("String");
+$n=repeatThreeTimes("String");
 echo $n;
