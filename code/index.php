d76eafdd3b85b6ce89310e71ed890dcad8d46b60
zaaalex <alexzorko@yandex.ru>
2022-09-24 10:57:41 +0200

    массивы 1

--- code/index.php
+++ code/index.php
@@ -1,21 +1,15 @@
 <?php
 
-function suum($c)
-{
-    $sum = 0;
-    while ($c > 0) {
-        $sum += $c % 10;
-        $c = intdiv($c,10);
+function arrayfill($s, $count){
+    $mas=[];
+    for ($i=0;$i<$count;++$i){
+    $mas[$i]=$s;
     }
-    return $sum;
+
+    return $mas;
 }
 
-$c=999;
-while ($c>10){
-    $c=suum($c);
-}
-
-echo $c;
+print_r(arrayfill("#",5));
 
 
 
