377672dfcba3dcdd0dcf781948a1b39dcdbb58cb
zaaalex <alexzorko@yandex.ru>
2022-09-26 22:26:11 +0200

    массивы 6

--- code/index.php
+++ code/index.php
@@ -1,9 +1,9 @@
 <?php
 
-$mas= array(
-    1 => 'name',
-    2 => 'surname',
-    3 => 'patronymic'
+$user= array(
+  "name"=>1,
+   "surname"=>2,
+   "patronymic"=>3
 );
 
-echo $mas[2], ' ', $mas[1], ' ', $mas[3];
+print_r(array_keys($user));
