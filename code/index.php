69a084cdab7d5af05a2a7777d342d8202d17892b
zaaalex <alexzorko@yandex.ru>
2022-09-26 22:32:08 +0200

    Конструкция if/else

--- code/index.php
+++ code/index.php
@@ -1,4 +1,18 @@
 <?php
 
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo $arr[count($arr)-1], " ", $arr[count($arr)-2];
+function check($a, $b){
+    if ($a+$b>10) return true;
+
+    return false;
+}
+
+function check2($a, $b){
+    if ($a+$b==10) return true;
+
+    return false;
+}
+
+
+//if ($test == 0) echo 'верно';
+
+
